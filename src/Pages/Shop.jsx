3a1827b9317a174741d56ba8978820ef37959ecd
Sunnatbek <Sunnatbek912@gmail.com>
2026-02-28 17:09:31 +0500

    design

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,24 @@
-const data = ['Iphone 12', 'Iphone 13', 'Iphone 15 pro', 'Iphone 17 pro max']
+const data = [
+	'Iphone 12',
+	'Iphone 13',
+	'Iphone 15 pro',
+	'Iphone 17 pro max',
+	'Galaxay S24',
+]
 
 function Shop() {
 	return (
 		<>
 			<h1>Shop</h1>
+			<li>
+				{data.map(phone => {
+					return (
+						<>
+							<li> {phone} </li>
+						</>
+					)
+				})}
+			</li>
 		</>
 	)
 }
